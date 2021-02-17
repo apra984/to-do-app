330c864d001fb02fafeb7ff6d3f74b1098650dd0
Akash Prakash <apra984@aucklanduni.ac.nz>
2021-02-17 18:33:14 +1300

    Add components to App

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,9 +1,19 @@
+import React, {useState} from "react";
 import './App.css';
+import Form from './components/Form';
+import TodoList from './components/TodoList';
 
 function App() {
+  const[inputText, setInputText] = useState("");
+  const[todos, setTodos] = useState([]);
+
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <header>
+      <h1>My To-Do List</h1>
+      </header>
+      <Form setInputText={setInputText}/>
+      <TodoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
